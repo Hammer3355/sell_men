b7e2fee881323216adef704851c9a747369bd1e8
hammer <ttkagentusolie@gmail.com>
2024-03-25 05:39:14 +0800

    Создана модель models.py

--- dev_sell_men/projects/models.py
+++ dev_sell_men/projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Tad(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
