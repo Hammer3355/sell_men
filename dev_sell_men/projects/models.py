5a2cddb360ee6ad3c7dc0ef9808507fdb6bed73d
hammer <ttkagentusolie@gmail.com>
2024-03-25 05:37:30 +0800

    Создана модель Project и Tag

--- dev_sell_men/projects/models.py
+++ dev_sell_men/projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tad(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)  # null=True, blank=True - Если поле не заполнено то значение null
+    featured_image = models.ImageField(upload_to='projects/%Y/%m/%d/', default="default.jpg")
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.ImageField(default=0)
+    vote_ratio = models.ImageField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
