9507fa260eeea56d2f398f6ac9069d4fdd486755
hammer <ttkagentusolie@gmail.com>
2024-05-21 07:02:52 +0800

    Редактирование моделей

--- dev_sell_men/projects/models.py
+++ dev_sell_men/projects/models.py
@@ -3,24 +3,33 @@ from users.models import Profile
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name="Тег")
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
 
 class Project(models.Model):
-    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)  # null=True, blank=True - Если поле не заполнено то значение null
-    featured_image = models.ImageField(upload_to='projects/%Y/%m/%d/', default="default.jpg")
-    demo_link = models.CharField(max_length=2000, null=True, blank=True)
-    source_link = models.CharField(max_length=2000, null=True, blank=True)
-    tags = models.ManyToManyField('Tag', blank=True)
-    vote_total = models.IntegerField(default=0)
-    vote_ratio = models.IntegerField(default=0)
-    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Владелец")
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")  # null=True, blank=True - Если поле не заполнено то значение null
+    featured_image = models.ImageField(upload_to='projects/%Y/%m/%d/', default="default.jpg", verbose_name="Изображение")
+    demo_link = models.CharField(max_length=2000, null=True, blank=True, verbose_name="Ссылка на демо")
+    source_link = models.CharField(max_length=2000, null=True, blank=True, verbose_name="Ссылка на источник")
+    tags = models.ManyToManyField('Tag', blank=True, verbose_name="Теги")
+    vote_total = models.IntegerField(default=0, verbose_name="Количество голосов")
+    vote_ratio = models.IntegerField(default=0, verbose_name="Рейтинг")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+        ordering = ['-created']
--- dev_sell_men/users/models.py
+++ dev_sell_men/users/models.py
@@ -4,28 +4,32 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Пользователь")
+    name = models.CharField(max_length=200, null=True, blank=True, verbose_name="Имя")
     email = models.EmailField(max_length=500, null=True, blank=True)
-    username = models.CharField(max_length=200, null=True, blank=True)
-    short_intro = models.CharField(max_length=200, null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    profile_image = models.ImageField(upload_to='profiles/', default="profiles/user-default.png")
-    social_ytube = models.CharField(max_length=200, null=True, blank=True)
-    social_website = models.CharField(max_length=200, null=True, blank=True)
-    social_telegram = models.CharField(max_length=200, null=True, blank=True)
-    social_vk = models.CharField(max_length=200, null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=200, null=True, blank=True, verbose_name="Имя пользователя")
+    short_intro = models.CharField(max_length=200, null=True, blank=True, verbose_name="Краткое описание")
+    bio = models.TextField(null=True, blank=True, verbose_name="О себе")
+    profile_image = models.ImageField(upload_to='profiles/', default="profiles/user-default.png", verbose_name="Аватар")
+    social_ytube = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ссылка на ютуб")
+    social_website = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ссылка на веб сайт")
+    social_telegram = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ссылка на телеграм")
+    social_vk = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ссылка на вконтакте")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
 
     def __str__(self):
         return f"{self.username}"
 
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
 
 class Skill(models.Model):
-    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Владелец")
+    name = models.CharField(max_length=200, null=True, blank=True, verbose_name="Название")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
 
     def __str__(self):
         return f"{self.name}"
